42ff19466a2101e08fcf152a4858fa489ab21f4b
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2022-07-16 21:49:18 -0400

    initial commit

---
+++ src/main/java/com/example/thn/brewery/Tsbb2bSfgBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.example.thn.brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tsbb2bSfgBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tsbb2bSfgBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/thn/brewery/Tsbb2bSfgBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thn.brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tsbb2bSfgBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
